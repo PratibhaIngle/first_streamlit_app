65e5b8a48a5625e9cdd07c60c3e8bbbb90c5c724
PratibhaIngle <154880583+PratibhaIngle@users.noreply.github.com>
2023-12-26 16:45:39 +0530

    Update streamlit_app.py
    Another Python Library in our Application

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit 
+import pandas
 streamlit.title("My parents new Healthy dinner now we will enjoy this time with family ");streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
 
